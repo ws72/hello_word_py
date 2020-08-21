51ae08c36e6ab2292aa3e0a948c7812dc3581e40
root <root@ip-10-0-1-223.ap-southeast-2.compute.internal>
2020-08-21 05:04:58 +0000

    updated tiny_app port to 80

--- tiny_app.py
+++ tiny_app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Afterpay!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port='80')
